2985bb30ecc8eee34a0633e4afbc961c07c22546
floydkim <for12gm@gmail.com>
2024-12-31 21:50:11 +0900

    feat: implement release history creation functionality

---
+++ cli/commands/createHistoryCommand/index.js
@@ -0,0 +1,28 @@
+const { program, Option } = require("commander");
+const { findAndReadConfigFile } = require("../../utils/fsUtils");
+const { createReleaseHistory } = require("./createReleaseHistory");
+
+program.command('create-history')
+    .description('Create a new release history for the binary app.')
+    .requiredOption('-v, --target-version <string>', 'target app binary version')
+    .addOption(new Option('-p, --platform <type>', 'platform').choices(['ios', 'android']).default('ios'))
+    .option('-i, --identifier <string>', 'additional characters to identify the release')
+    .option('-c, --config <path>', 'set config file name (JS/TS)', 'codepush.config.ts')
+    /**
+     * @param {Object} options
+     * @param {string} options.targetVersion
+     * @param {string} options.platform
+     * @param {string} options.identifier
+     * @param {string} options.config
+     * @return {void}
+     */
+    .action(async (options) => {
+        const config = findAndReadConfigFile(process.cwd(), options.config);
+
+        await createReleaseHistory(
+            options.targetVersion,
+            config.setReleaseHistory,
+            options.platform,
+            options.identifier
+        );
+    });
