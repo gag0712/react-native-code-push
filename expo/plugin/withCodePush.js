b7823e6fd4b7027a4f53e735da702ac6581e96c1
Floyd Kim <for12gm@gmail.com>
2025-09-26 14:26:29 +0900

    fix(Expo): prebuild not working (#89)

--- expo/plugin/withCodePush.js
+++ expo/plugin/withCodePush.js
@@ -1,10 +1,9 @@
 const { createRunOncePlugin } = require('expo/config-plugins');
-const { withAndroidBuildScriptDependency, withAndroidMainApplicationDependency } = require('./withCodePushAndroid');
+const { withAndroidMainApplicationDependency } = require('./withCodePushAndroid');
 const { withIosBridgingHeader, withIosAppDelegateDependency } = require('./withCodePushIos');
 const pkg = require('../../package.json');
 
 const withCodePush = (config) => {
-  config = withAndroidBuildScriptDependency(config);
   config = withAndroidMainApplicationDependency(config);
   config = withIosBridgingHeader(config);
   config = withIosAppDelegateDependency(config);
