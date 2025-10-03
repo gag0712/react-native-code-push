a65c085bc241e5178a9e0240ca1493d286e6e5d6
Floyd Kim <for12gm@gmail.com>
2025-10-03 19:04:58 +0900

    chore(Example): Updated the example app to use the local package source (#90)
    * chore(Example): update configuration to link local package
    
    * chore: update package.json to include react-native and source fields
    
    * chore: update package.json to change "typings" to "types"

--- Examples/CodePushDemoApp/metro.config.js
+++ Examples/CodePushDemoApp/metro.config.js
@@ -1,4 +1,8 @@
+const path = require('path');
 const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const projectRoot = __dirname;
+const workspaceRoot = path.resolve(projectRoot, '../..');
 
 /**
  * Metro configuration
@@ -6,6 +10,18 @@ const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
  *
  * @type {import('@react-native/metro-config').MetroConfig}
  */
-const config = {};
+const config = {
+  projectRoot: projectRoot,
+  watchFolders: [workspaceRoot],
+  resolver: {
+    // Always resolve deps from the example app to avoid module duplication.
+    extraNodeModules: new Proxy(
+      {},
+      {
+        get: (_, name) => path.join(projectRoot, 'node_modules', name),
+      },
+    ),
+  },
+};
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+module.exports = mergeConfig(getDefaultConfig(projectRoot), config);
