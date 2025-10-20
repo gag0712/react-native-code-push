87acf308ec96901c014258f6d6f3751d7bfd0ac0
Floyd Kim <for12gm@gmail.com>
2025-10-20 20:55:59 +0900

    fix(CLI): jest error while resolving js modules

--- cli/jest.config.cjs
+++ cli/jest.config.cjs
@@ -2,7 +2,7 @@
 module.exports = {
   rootDir: __dirname,
   testEnvironment: 'node',
-  // resolver: '<rootDir>/jest.resolver.cjs',
+  resolver: '<rootDir>/jest.resolver.cjs',
   transform: {
     '^.+\\.(t|j)s$': ['babel-jest', { configFile: '../babel.config.js' }],
   },
